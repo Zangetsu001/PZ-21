e12880792db025c830c4d913d861bc4d27258e58
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-02-01 21:33:44 +0200

    Add files via upload

---
+++ Calculate1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Calculate1
+    {
+        /// <summary>
+        /// Поля класса
+        /// </summary>
+        public double Number1;
+        public double Number2;
+
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Calculate1() { } 
+        /// <summary>
+        /// Конструктор с 1 параметром
+        /// </summary>
+        /// <param name="Number1"></param>
+        public Calculate1(double Number1)
+        {
+            this.Number1 = Number1;
+        }
+        /// <summary>
+        /// Конструктор с 2 параметрами
+        /// </summary>
+        /// <param name="Number1"></param>
+        /// <param name="Number2"></param>
+        public Calculate1(double Number1, double Number2) : this(Number1)
+        {
+            this.Number2 = Number2;
+        }
+
+        /// <summary>
+        /// Вывод чисел с помощью метода Info
+        /// </summary>
+        /// <returns></returns>
+        public string Info()
+        {
+            return $"Первое число: {Number1}; " +
+                $"Второе число: {Number2}";
+        }
+        public double SquareRoot()
+        {
+            double product = Number1 * Number2;
+            return product >= 0 ? Math.Sqrt(product) : Double.NaN;
+        }
+        /// <summary>
+        /// Вывод чисел с использованием метода ToString
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"Первое число: {Number1}," +
+                $"Второе число: {Number2}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Создание объекта без параметров и присваивание значений полям
+            Calculate1 calc1 = new Calculate1();
+            calc1.Number1 = 3.6;
+            calc1.Number2 = 8.5;
+            Console.WriteLine(calc1.Info());
+            Console.WriteLine($"Вывод: {calc1.SquareRoot()}");
+
+            
+            //Создание обьекта с явной инициализацией
+            Calculate1 calc2 = new Calculate1(3.6, 4.7);
+            Console.WriteLine(calc2.Info());
+            Console.WriteLine($"Вывод: {calc2.SquareRoot()}");
+
+            //Ввод значений через клавиатуру
+            Console.Write("Введите первое число: ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            //Вывод значений
+            Calculate1 calc3 = new Calculate1(number1, number2);
+            Console.WriteLine(calc3.Info());
+            Console.WriteLine($"Вывод: {calc3.SquareRoot()}");
+
+            Console.Read();
+
+        }
+    }
+}
