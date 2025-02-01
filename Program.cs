fcd89116234aeb2236f07ae65359251a64eefb1c
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-02-01 21:34:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создание объекта без параметров и присваивание значений полям
+            Time time1 = new Time();
+            time1.hours = 5;
+            time1.minutes = 23;
+            time1.seconds = 43;
+            Console.WriteLine(time1.Info());
+            Console.WriteLine("Общее количество минут: " + time1.TotalMinutes());
+            Console.WriteLine("Общее количество секунд: " + time1.TotalSeconds());
+
+            //Создание объекта с явной инициализацией
+            Time time2 = new Time(3, 42, 75);
+            Console.WriteLine(time2.Info());
+            Console.WriteLine("Общее количество минут: " + time1.TotalMinutes());
+            Console.WriteLine("Общее количество секунд: " + time1.TotalSeconds());
+
+            //Ввод значений с клавиатуры и создание объекта
+            Console.Write("Введите количество часов: ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество минут: ");
+            int minutes = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество секунд: ");
+            int seconds = Convert.ToInt32(Console.ReadLine());
+
+            //Вывод значения
+            Time time3 = new Time(hours, minutes, seconds);
+            Console.WriteLine(time3.Info());
+            Console.WriteLine("Общее количество минут: " + time1.TotalMinutes());
+            Console.WriteLine("Общее количество секунд: " + time1.TotalSeconds());
+        }
+    }
+}
---
+++ Time.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Time
+    {
+        /// <summary>
+        /// Создание полей
+        /// </summary>
+        public int hours;
+        public int minutes;
+        public int seconds;
+
+        /// <summary>
+        /// Создание пустого параметров
+        /// </summary>
+        public Time() { }
+        /// <summary>
+        /// Конструктор с 1 параметром
+        /// </summary>
+        /// <param name="hours"></param>
+        public Time(int hours)
+        {
+            this.hours = hours;
+        }
+        /// <summary>
+        /// Конструктор с 2 параметрами
+        /// </summary>
+        /// <param name="hours"></param>
+        /// <param name="minutes"></param>
+        public Time(int hours, int minutes) : this(hours)
+        {
+            this.minutes = minutes;
+        }
+        /// <summary>
+        /// Конструктор с 3 параметрами
+        /// </summary>
+        /// <param name="hours"></param>
+        /// <param name="minutes"></param>
+        /// <param name="seconds"></param>
+        public Time(int hours, int minutes, int seconds) : this(hours, minutes)
+        {
+            this.seconds = seconds;
+        }
+        /// <summary>
+        /// Функция формирования строки с информацией об объекте
+        /// </summary>
+        /// <returns>Info</returns>
+        public string Info()
+        {
+            return $"Время: {hours:d2}:{minutes:d2}:{seconds:d2}";
+        }
+        /// <summary>
+        /// Функция обработки значений полей: вычислить общее количество минут
+        /// </summary>
+        /// <returns>Total_Minutes</returns>
+        public int TotalMinutes()
+        {
+            return hours * 60 + minutes;
+        }
+        /// <summary>
+        /// Функция обработки значений полей: вычислить общее количество секунд
+        /// </summary>
+        /// <returns>Total_seconds</returns>
+        public int TotalSeconds()
+        {
+            return (hours * 3600) + (minutes * 60) + seconds;
+        }
+        /// <summary>
+        /// Вывод методом ToString
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"Time [Hours = {hours}, Minutes = {minutes}, Seconds = {seconds}]";
+        }
+    }
+}
